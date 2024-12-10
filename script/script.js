c703306ef250508ef6211d05db21387e1efc0488
MrSwizzer <155651796+MrSwizzer@users.noreply.github.com>
2024-12-10 17:44:35 +0100

    Add Mute Button

--- script/script.js
+++ script/script.js
@@ -2,46 +2,55 @@ let timer;
 let seconds = 10;
 let isStretching = false;
 let round = 1;
+let isMuted = false;
 
 function playSound1() {
-    let audio = document.getElementById("sound1");
-    audio.play();
+	if (!isMuted) {
+		let audio = document.getElementById('sound1');
+		audio.play();
+	}
 }
 
 function playSound2() {
-    let audio = document.getElementById("sound2");
-    audio.play();
+	if (!isMuted) {
+		let audio = document.getElementById('sound2');
+		audio.play();
+	}
 }
 
 function startTimer() {
-    if (!isStretching) {
-        isStretching = true;
-        timer = setInterval(function () {
-            document.getElementById("countdown").innerText = "Countdown: " + seconds + " Sekunden";
+	if (!isStretching) {
+		isStretching = true;
+		timer = setInterval(function () {
+			document.getElementById('countdown').innerText = 'Countdown: ' + seconds + ' Sekunden';
 
-            if (seconds > 0) {
-                seconds--;
-            } else {
-                if (round % 2 !== 0) {
-                    playSound1();
-                    seconds = 30;
-                } else {
-                    playSound2();
-                    seconds = 10;
-                }
-                round++;
-            }
-
-        }, 1000);
-    }
-
+			if (seconds > 0) {
+				seconds--;
+			} else {
+				if (round % 2 !== 0) {
+					playSound1();
+					seconds = 30;
+				} else {
+					playSound2();
+					seconds = 10;
+				}
+				round++;
+			}
+		}, 1000);
+	}
 }
 
 function stopTimer() {
-    clearInterval(timer);
-    isStretching = false;
-    seconds = 10;
-    round = 1;
-    document.getElementById("countdown").innerText = "Stretching beendet!";
-    timer = null;
+	clearInterval(timer);
+	isStretching = false;
+	seconds = 10;
+	round = 1;
+	document.getElementById('countdown').innerText = 'Stretching beendet!';
+	timer = null;
+}
+
+function toggleMute() {
+	isMuted = !isMuted;
+	let muteButton = document.getElementById('muteButton');
+	muteButton.innerText = isMuted ? 'Unmute Notifications' : 'Mute Notifications';
 }
